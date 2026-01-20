cddbffb1f1712648b0c91a0acd97cac7aa23d076
Youssef Fathy <144230136+fathyyoussef1310@users.noreply.github.com>
2026-01-20 22:27:01 +0200

    Onboarding_Screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-
     return ScreenUtilInit(
       designSize: const Size(393, 852),
       minTextAdapt: true,
