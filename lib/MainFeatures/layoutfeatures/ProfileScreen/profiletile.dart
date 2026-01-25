a3329ce98e3456e043f6282fa2b18d50103f7aec
Yousef Sham3a <yousefshamaa1231@gmail.com>
2026-01-25 02:44:54 +0200

    profilescreen

---
+++ lib/MainFeatures/layoutfeatures/ProfileScreen/profiletile.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ProfileTile extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String? value;
+  final Color color;
+  final VoidCallback? onTap;
+
+  const ProfileTile({
+    super.key,
+    required this.icon,
+    required this.title,
+    this.value,
+    this.color = Colors.black,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(icon, color: color),
+      title: Text(title, style: TextStyle(color: color)),
+      trailing: value != null
+          ? Text(value!, style: const TextStyle(color: Colors.grey))
+          : const Icon(Icons.arrow_forward_ios, size: 16),
+      onTap: onTap,
+    );
+  }
+}
